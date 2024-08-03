5e150e48480555d35e6311ddb60292fb9886f04f
Igor Koloniuk <koloniuk@gmail.com>
2024-08-03 12:08:44 +0200

    (fix) add httpclient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,27 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {ShellComponent} from "../views/shell/shell.component";
-import {ButtonModule} from "primeng/button";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {NgxsModule} from "@ngxs/store";
-import {GlobalState} from "./store_xs/store.state";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ShellComponent } from '../views/shell/shell.component';
+import { ButtonModule } from 'primeng/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxsModule } from '@ngxs/store';
+import { GlobalState } from './store_xs/store.state';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ShellComponent,
     NgxsModule.forRoot([GlobalState]),
     ButtonModule,
-    BrowserAnimationsModule
-
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [provideHttpClient()],
   exports: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
