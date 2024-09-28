09de5e0268279d72d8c9e4159c22d33004c18fd9
xkoloniuk <koloniuk@gmail.com>
2024-09-28 20:53:28 +0200

    (feat) WIP remove number-cq-tm-template, add lodash

--- src/components/sample-card/sample-card.component.ts
+++ src/components/sample-card/sample-card.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ChipModule } from 'primeng/chip';
+import { AsyncPipe, NgForOf, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-sample-card',
   templateUrl: './sample-card.component.html',
   styleUrls: ['./sample-card.component.scss'],
   standalone: true,
+  imports: [ChipModule, NgIf, NgForOf, AsyncPipe],
 })
 export class SampleCardComponent {}
--- src/components/well-template/well-template.component.ts
+++ src/components/well-template/well-template.component.ts
@@ -1,14 +1,33 @@
 import { Component, Input } from '@angular/core';
 import { DecimalPipe, JsonPipe } from '@angular/common';
-import { NumberCqTmTemplateComponent } from '../number-cq-tm-template/number-cq-tm-template.component';
+import { qPCRrecord } from '../../interfaces/interface';
+import _ from 'lodash';
 
 @Component({
   selector: 'app-well-template',
   standalone: true,
-  imports: [JsonPipe, DecimalPipe, NumberCqTmTemplateComponent],
+  imports: [JsonPipe, DecimalPipe],
   templateUrl: './well-template.component.html',
   styleUrl: './well-template.component.scss',
 })
 export class WellTemplateComponent {
-  @Input({ required: true }) wellValue: any;
+  private _wellValue: { cq: null | number; tm: null | number } = {
+    cq: null,
+    tm: null,
+  };
+
+  @Input()
+  set wellValue(value: qPCRrecord) {
+    const cq = _.isNumber(value.Cq) ? +value.Cq : null;
+    const tm =
+      !!value['Melt Temperature'] && _.isNumber(value['Melt Temperature'])
+        ? +value['Melt Temperature']
+        : null;
+
+    this._wellValue = { cq, tm };
+  }
+
+  get wellValue() {
+    return this._wellValue;
+  }
 }
