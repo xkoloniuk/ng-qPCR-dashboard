152065e1b66fd5f8c33c505c82d7a92604ef16a6
xkoloniuk <koloniuk@gmail.com>
2024-10-26 00:09:16 +0200

    (feat) reorder colors

--- src/variables/colors.ts
+++ src/variables/colors.ts
@@ -1,16 +1,16 @@
 export const distinctColors: string[] = [
-  '#FF6B6B', // Vibrant Red
-  '#4ECDC4', // Turquoise
-  '#FFA07A', // Light Salmon
-  '#7FDBFF', // Azure
-  '#FFC300', // Vivid Yellow
   '#82C91E', // Lime Green
-  '#9775FA', // Bright Purple
-  '#FF8ED4', // Pink
-  '#FFD166', // Marigold
-  '#06D6A0', // Caribbean Green
   '#45AAF2', // Picton Blue
   '#FC5C65', // Watermelon
+  '#4ECDC4', // Turquoise
+  '#FFA07A', // Light Salmon
+  '#9775FA', // Bright Purple
+  '#7FDBFF', // Azure
+  '#FFC300', // Vivid Yellow
+  '#06D6A0', // Caribbean Green
+  '#FF6B6B', // Vibrant Red
+  '#FF8ED4', // Pink
+  '#FFD166', // Marigold
   '#26DE81', // Algal Fuel
   '#FD9644', // Mandarin
   '#5F27CD', // Blue Violet
