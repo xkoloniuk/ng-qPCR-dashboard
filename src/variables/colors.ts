5a3b650e5f46e072d974ca33edc64c3b45c0924c
xkoloniuk <koloniuk@gmail.com>
2024-10-25 23:58:41 +0200

    (feat) add color service

---
+++ src/variables/colors.ts
@@ -0,0 +1,17 @@
+export const distinctColors: string[] = [
+  '#FF6B6B', // Vibrant Red
+  '#4ECDC4', // Turquoise
+  '#FFA07A', // Light Salmon
+  '#7FDBFF', // Azure
+  '#FFC300', // Vivid Yellow
+  '#82C91E', // Lime Green
+  '#9775FA', // Bright Purple
+  '#FF8ED4', // Pink
+  '#FFD166', // Marigold
+  '#06D6A0', // Caribbean Green
+  '#45AAF2', // Picton Blue
+  '#FC5C65', // Watermelon
+  '#26DE81', // Algal Fuel
+  '#FD9644', // Mandarin
+  '#5F27CD', // Blue Violet
+];
