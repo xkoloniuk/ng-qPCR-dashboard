8caf8f41d1723ee821781a9e67a454fc74b9ee50
xkoloniuk <koloniuk@gmail.com>
2024-10-26 13:32:11 +0200

    (feat) introduce target-tag component

---
+++ src/components/target-tag/target-tag.component.ts
@@ -0,0 +1,30 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  input,
+  OnDestroy,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ColorService } from '../../services/color.service';
+
+@Component({
+  selector: 'app-target-tag',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './target-tag.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TargetTagComponent implements OnDestroy {
+  public target = input('');
+  #colorService = inject(ColorService);
+
+  public bgColor = computed(() => {
+    return this.#colorService.generatePalette(this.target());
+  });
+
+  public ngOnDestroy(): void {
+    this.#colorService.resetPalette();
+  }
+}
