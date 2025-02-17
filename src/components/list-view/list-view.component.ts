b8ee2531b3831df0fac11913f1643fb1487ccc60
xkoloniuk <koloniuk@gmail.com>
2025-02-17 20:02:15 +0100

    (fix) use buttons for open table/plate

--- src/components/list-view/list-view.component.ts
+++ src/components/list-view/list-view.component.ts
@@ -1,27 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { AsyncPipe, DatePipe, NgForOf, NgIf } from '@angular/common';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { TableModule } from 'primeng/table';
-import { TargetCardComponent } from '../target-card/target-card.component';
 import { qPCRFile } from '../../interfaces/interface';
 import { Store } from '@ngxs/store';
 import { GlobalState } from '../../app/store_xs/store.state';
+import { Button } from 'primeng/button';
 
 @Component({
   selector: 'app-list-view',
   templateUrl: './list-view.component.html',
   styleUrls: ['./list-view.component.scss'],
   standalone: true,
-  // changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [
-    AsyncPipe,
-    NgIf,
-    NgForOf,
-    TableModule,
-    DatePipe,
-    RouterLink,
-    TargetCardComponent,
-  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [TableModule, DatePipe, RouterLink, Button],
 })
 export class ListViewComponent implements OnInit {
   public target?;
